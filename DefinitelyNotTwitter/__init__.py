6f9efe9bf00c1fd273d8c08b11c127eaec8535e2
Nick Hafkemeyer <mail@nhafkemeyer.com>
2018-05-16 16:33:24 +0200

    edited __init__.py, database.py and schema.sql

--- DefinitelyNotTwitter/__init__.py
+++ DefinitelyNotTwitter/__init__.py
@@ -1,17 +1,18 @@
 import os
 
 from flask import Flask
+from . import database as db
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE = os.path.join(app.instance_path, 'DefinitelyNotTwitter.sqlite'),
-
-
+    )
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
+    db.init_app(app)
     return app
--- DefinitelyNotTwitter/database.py
+++ DefinitelyNotTwitter/database.py
@@ -17,5 +17,22 @@ def get_db():
 def close_db(e=None):
     db = g.pop('db', None)
 
-    is db is not None:
+    if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear existing data and create new tables"""
+    init_db()
+    click.echo('Initialized database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
