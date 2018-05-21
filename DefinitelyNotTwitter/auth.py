8f8ae7a2cacac5c0a8510f197d1f5070a2a6eb0f
Nick Hafkemeyer <mail@nhafkemeyer.com>
2018-05-21 11:51:59 +0200

    added decorator login_required and load_logged_in_user function

--- DefinitelyNotTwitter/auth.py
+++ DefinitelyNotTwitter/auth.py
@@ -1,11 +1,12 @@
 import functools
 
 from flask import(
-    Blueprint, flash, redirect, render_template, request, session, url_for
+    Blueprint, flash, redirect, render_template, request, session, url_for, g
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from DefinitelyNotTwitter.database import get_db
+from . import user
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -67,8 +68,33 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('user.show_profile', id = session['user_id']))
 
         flash(error)
 
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+    return wrapped_view
